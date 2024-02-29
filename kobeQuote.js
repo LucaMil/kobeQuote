446e439e935fd562f108f5135167a4c8f772b285
Luca Milanese <lucamilanese01@gmail.com>
2024-02-29 17:10:31 +0000

    add object

--- kobeQuote.js
+++ kobeQuote.js
@@ -1,7 +1,11 @@
-const rng = Math.floor(Math.random() * 10);
+const rng = (num) {
+  return Math.floor(Math.random() * num);
+}
 
-const quotes = ["Job finished?", "I don't think so.", "Job's not finished.", "They shook the tree.", "And the mamba fell out.", "Dance beautifully in the box you're comfortable dancing in.",
-"Be embarrassed when rejected rather than when you achieved nothing.", "Don't get too high or too low. Stay centered.", "Failure is non existant.",
-"What's there to be happy about?"];
-
+const fullQuote = {
+    question: ["You're up 2-0. Are you not happy?", "What's the story?", "How would you characterize this season?", "Still waiting for a big smile out of you?"],
+    quote1: ["Job finished?", "I don't think so.", "Job's not finished.", "They shook the tree.", "Dance beautifully in the box you're comfortable dancing in.",],
+    quote2: ["Be embarrassed when rejected rather than when you achieved nothing.", "Don't get too high or too low. Stay centered.", "Failure is non existant.",
+    "What's there to be happy about?", "And the mamba fell out."]
+};
 
