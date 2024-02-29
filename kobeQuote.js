4d5359ab53663630e3c289b391253cd5009f6288
Luca Milanese <lucamilanese01@gmail.com>
2024-02-29 16:18:46 +0000

    add array for quotes

--- kobeQuote.js
+++ kobeQuote.js
@@ -1,1 +1,7 @@
 const rng = Math.floor(Math.random() * 10);
+
+const quotes = ["Job finished?", "I don't think so.", "Job's not finished.", "They shook the tree.", "And the mamba fell out.", "Dance beautifully in the box you're comfortable dancing in.",
+"Be embarrassed when rejected rather than when you achieved nothing.", "Don't get too high or too low. Stay centered.", "Failure is non existant.",
+"What's there to be happy about?"];
+
+
